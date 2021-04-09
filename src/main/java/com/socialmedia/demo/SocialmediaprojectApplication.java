f3c69e466bac0d2e0f58578e7ababb615d6f66be
Brian Ortiz <bortiznine@gmail.com>
2021-04-09 11:57:36 -0500

    project skeleton

---
+++ src/main/java/com/socialmedia/demo/SocialmediaprojectApplication.java
@@ -0,0 +1,13 @@
+package com.socialmedia.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialmediaprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SocialmediaprojectApplication.class, args);
+    }
+
+}
