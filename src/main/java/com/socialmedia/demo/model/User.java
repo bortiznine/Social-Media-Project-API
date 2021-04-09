ecdfdc87c9d14f2a2f65cd2ba2fdaee658db1bc1
Brian Ortiz <bortiznine@gmail.com>
2021-04-09 15:12:53 -0500

    added controller, exception, model,repository,security, and service packages

---
+++ src/main/java/com/socialmedia/demo/model/User.java
@@ -0,0 +1,118 @@
+package com.socialmedia.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.annotations.LazyCollection;
+import org.hibernate.annotations.LazyCollectionOption;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name="users")
+public class User {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+
+    @Column(unique = true)
+    private String emailAddress;
+
+    @Column
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    private String password;
+
+    // one user can have only one profile
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name="profile_id", referencedColumnName="id")
+    private UserProfile userProfile;
+
+    // user can have more than one recipe
+    @OneToMany(mappedBy="user")
+    @LazyCollection(LazyCollectionOption.FALSE) // This means that the Recipe.user property holds the logic for mapping this relationship
+    private List<Recipe> recipeList;
+
+    // user can have more than one category
+    @OneToMany(mappedBy="user")
+    @LazyCollection(LazyCollectionOption.FALSE)  // This means that the Category.user property holds the logic for mapping this relationship
+    private List<Category> categoryList;
+
+    public UserProfile getUserProfile() {
+        return userProfile;
+    }
+
+    public void setUserProfile(UserProfile userProfile) {
+        this.userProfile = userProfile;
+    }
+
+    public List<Recipe> getRecipeList() {
+        return recipeList;
+    }
+
+    public void setRecipeList(List<Recipe> recipeList) {
+        this.recipeList = recipeList;
+    }
+
+    public List<Category> getCategoryList() {
+        return categoryList;
+    }
+
+    public void setCategoryList(List<Category> categoryList) {
+        this.categoryList = categoryList;
+    }
+
+    public User() {
+    }
+
+    public User(Long id, String username, String emailAddress, String password) {
+        this.id = id;
+        this.username = username;
+        this.emailAddress = emailAddress;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String userName) {
+        this.username = userName;
+    }
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", userName='" + username + '\'' +
+                ", emailAddress='" + emailAddress + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/socialmedia/demo/model/UserProfile.java
@@ -0,0 +1,82 @@
+package com.socialmedia.demo.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name="profiles")
+public class UserProfile {
+
+    @Id
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private String profileDescription;
+
+    @JsonIgnore
+    @OneToOne(mappedBy = "userProfile")
+    private User user;
+
+    public UserProfile() {
+    }
+
+    public UserProfile(Long id, String firstName, String lastName, String profileDescription) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.profileDescription = profileDescription;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getProfileDescription() {
+        return profileDescription;
+    }
+
+    public void setProfileDescription(String profileDescription) {
+        this.profileDescription = profileDescription;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "UserProfile{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", profileDescription='" + profileDescription + '\'' +
+                '}';
+    }
+}
